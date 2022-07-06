a09191574ce655f67e6d943f6f58123a619cda41
Dawid Badurek <dawidbadurek@wp.pl>
2022-07-06 21:20:23 +0200

    Apply eslint

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,24 +1,25 @@
+import React from 'react';
 import { useRef } from 'react';
 import '../styles/Cell.css'
 import { Coordinates } from './types';
 
 export interface CellProprs {
-    cords: Coordinates;
-    onClick: (cords: Coordinates, position: Coordinates) => void
-    active: boolean
+  cords: Coordinates;
+  onClick: (cords: Coordinates, position: Coordinates) => void
+  active: boolean
 }
 
 export function Cell(props: CellProprs) {
-    const cell = useRef<HTMLDivElement>(null);
+  const cell = useRef<HTMLDivElement>(null);
 
-    const onClick = () => {
-        if (cell.current === null)
-            return;
+  const onClick = () => {
+    if (cell.current === null)
+      return;
 
-        const position = cell.current.getBoundingClientRect()
-        props.onClick(props.cords, { x: position.right, y: position.top })
-    }
+    const position = cell.current.getBoundingClientRect()
+    props.onClick(props.cords, { x: position.right, y: position.top })
+  }
 
 
-    return <div ref={cell} className={"Cell " + (props.active ? "active" : "")} onClick={onClick}></div >
+  return <div ref={cell} className={"Cell " + (props.active ? "active" : "")} onClick={onClick}></div >
 }
--- src/components/SudokuBoard.tsx
+++ src/components/SudokuBoard.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react'
 import '../styles/SudokuBoard.css'
 import { Cell } from './Cell'
@@ -5,41 +6,41 @@ import { Keyboard } from './Keyboard'
 import { SudokuBoardState, Coordinates, KeyboardState } from './types'
 
 export function SudokuBoard() {
-    const [activeCell, setActiveCell] = useState<SudokuBoardState>({ keyboardPosition: { x: 0, y: 0 }, keyboardState: KeyboardState.HIDDEN })
+  const [activeCell, setActiveCell] = useState<SudokuBoardState>({ keyboardPosition: { x: 0, y: 0 }, keyboardState: KeyboardState.HIDDEN })
 
-    const isActive = (cords: Coordinates) => {
-        return activeCell.cords?.x === cords.x && activeCell.cords?.y === cords.y
+  const isActive = (cords: Coordinates) => {
+    return activeCell.cords?.x === cords.x && activeCell.cords?.y === cords.y
+  }
+
+  const onCellClick = (cords: Coordinates, position: Coordinates) => {
+    if (isActive(cords))
+      return setActiveCell({ keyboardPosition: activeCell.keyboardPosition, keyboardState: KeyboardState.HIDE });
+
+    return setActiveCell({ cords: cords, keyboardPosition: position, keyboardState: nextKeyboardState(activeCell.keyboardState) })
+  }
+
+  const onKeyboardClick = (digit: number) => {
+    console.log(digit)
+    // TODO: handle the keyboard click
+  }
+
+
+  let cells: JSX.Element[] = []
+  for (let y = 0; y < 9; y++) {
+    let row: JSX.Element[] = []
+
+    for (let x = 0; x < 9; x++) {
+      row.push(<Cell active={isActive({ x, y })} cords={{ x, y }} onClick={onCellClick} key={`${x}${y}`} ></Cell >)
     }
+    cells.push(<div className='row' key={y}>{row}</div>)
+  }
 
-    const onCellClick = (cords: Coordinates, position: Coordinates) => {
-        if (isActive(cords))
-            return setActiveCell({ keyboardPosition: activeCell.keyboardPosition, keyboardState: KeyboardState.HIDE });
-
-        return setActiveCell({ cords: cords, keyboardPosition: position, keyboardState: nextKeyboardState(activeCell.keyboardState) })
-    }
-
-    const onKeyboardClick = (digit: number) => {
-        console.log(digit)
-        // TODO: handle the keyboard click
-    }
-
-
-    let cells: JSX.Element[] = []
-    for (let y = 0; y < 9; y++) {
-        let row: JSX.Element[] = []
-
-        for (let x = 0; x < 9; x++) {
-            row.push(<Cell active={isActive({ x, y })} cords={{ x, y }} onClick={onCellClick} key={`${x}${y}`} ></Cell >)
-        }
-        cells.push(<div className='row' key={y}>{row}</div>)
-    }
-
-    return <div className='SudokuBoard'>
-        {cells}
-        <Keyboard state={activeCell.keyboardState} position={activeCell.keyboardPosition} onKeyboardClick={onKeyboardClick}></Keyboard>
-    </div>
+  return <div className='SudokuBoard'>
+    {cells}
+    <Keyboard state={activeCell.keyboardState} position={activeCell.keyboardPosition} onKeyboardClick={onKeyboardClick}></Keyboard>
+  </div>
 }
 
 function nextKeyboardState(currState: KeyboardState): KeyboardState {
-    return currState === KeyboardState.SHIFT || currState === KeyboardState.SHOW ? KeyboardState.SHIFT : KeyboardState.SHOW;
+  return currState === KeyboardState.SHIFT || currState === KeyboardState.SHOW ? KeyboardState.SHIFT : KeyboardState.SHOW;
 }
